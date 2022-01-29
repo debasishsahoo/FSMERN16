412287a9309543196f03e67ba71e7d9b90c1e4cf
debasishsahoo96 <debasish.sahoo96@gmail.com>
2022-01-29 18:06:07 +0530

    Second Commite

--- server/APP/index.js
+++ server/APP/index.js
@@ -1,17 +1,11 @@
-/**
- * #this place is for all the libs,CORS,env
- */
 const express = require('express');
-/**
- * #this place is for all the modules like routes,db,configs
- */
+const dotenv = require('dotenv');
 const UserRoutes = require('../ROUTERS/user.routes')
 const PostRoutes = require('../ROUTERS/post.routes')
 const AdminRoutes = require('../ROUTERS/admin.routes')
-const PORT = 7000;
-/**
- * CREATE APP  & IT IS THE SINGLE ENTRY FOR APPLICATION
- */
+dotenv.config()
+const PORT = process.env.PORT;
+
 const app = express();
 
 
---
+++ server/DATABASES/DBConnect.Mongo.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
