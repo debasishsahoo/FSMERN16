389e7467469bbea63b41893f504f459cd4d5960b
debasishsahoo96 <debasish.sahoo96@gmail.com>
2022-02-03 17:06:43 +0530

    Commmit User

--- server/CONTROLLERS/user.controller.js
+++ server/CONTROLLERS/user.controller.js
@@ -1,8 +1,41 @@
+const UserModel = require('../models/user.model')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
+dotenv.config()
+const Encode_Key = `${process.env.SECRET_KEY}`
+const Hash_Key = parseInt(`${process.env.HASH_KEY}`)
+
 
 const UserCTRL = {
     signup: async (req, res, next) => {
-        console.log('calling')
-        res.send('Calling from Signup')
+        const { name, email, password, mobile } = req.body
+        try {
+            const oldUser = await UserModel.findOne(email);
+            if (oldUser) {
+                return res.status(400).json({
+                    message: "Email already Exists",
+                });
+            }
+            const HasedPassword = await bcrypt.hash(password, Hash_Key)
+            const SendData = await UserModel.create({
+                name,
+                email,
+                password: HasedPassword,
+                mobile
+            });
+            res.status(201).json({
+                message: "User Created",
+                user: SendData
+            })
+        }
+        catch (error) {
+            res.status(404).json({
+                message: error.message,
+            })
+        }
+
     },
     signin: async (req, res, next) => {
         res.send('Calling from Signin')
