09e0664f029db51d3e8367d59ac69ec405ebd5e9
debasishsahoo96 <debasish.sahoo96@gmail.com>
2022-02-10 18:49:43 +0530

    HTML

--- client/src/components/auth/signin.js
+++ client/src/components/auth/signin.js
@@ -1,5 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
 function SignIn() {
-    return (<div>SignIn</div>)
+    return (<div className="container">
+        <div className="row">
+            <div className="col-md-6 offset-md-3">
+                <div className="signup-form">
+                    <div className="mt-5 border p-4 bg-light shadow">
+                        <h4 className="mb-5 text-secondary">Welcome Back</h4>
+                        <div className="row">
+                            <div className="mb-3 col-md-12">
+                                <label>Email<span className="text-danger">*</span></label>
+                                <input type="email" name="email" className="form-control" placeholder="Enter Email" />
+                            </div>
+                            <div className="mb-3 col-md-12">
+                                <label>Password<span className="text-danger">*</span></label>
+                                <input type="password" name="password" className="form-control" placeholder="Enter Password" />
+                            </div>
+                            <div className="col-md-12">
+                                <button className="btn btn-primary float-end">Signin Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    <p className="text-center mt-3 text-secondary">Have not account yet? <Link to='/signup'>Signup</Link></p>
+                </div>
+            </div>
+        </div>
+    </div>)
 
 }
 
--- client/src/components/auth/signup.js
+++ client/src/components/auth/signup.js
@@ -1,5 +1,53 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 function SignUp() {
-    return (<div>SignUp</div>)
+
+
+
+
+
+
+
+
+    return (<div className="container">
+        <div className="row">
+            <div className="col-md-6 offset-md-3">
+                <div className="signup-form">
+                    <div action="" className="mt-5 border p-4 bg-light shadow">
+                        <h4 className="mb-5 text-secondary">Create Your Account</h4>
+                        <div className="row">
+                            <div className="mb-3 col-md-6">
+                                <label>Name<span className="text-danger">*</span></label>
+                                <input type="text" name="name" className="form-control" placeholder="Enter First Name" />
+                            </div>
+                            <div className="mb-3 col-md-12">
+                                <label>Email<span className="text-danger">*</span></label>
+                                <input type="email" name="email" className="form-control" placeholder="Enter Email" />
+                            </div>
+                            <div className="mb-3 col-md-6">
+                                <label>Mobile<span className="text-danger">*</span></label>
+                                <input type="text" name="mobile" className="form-control" placeholder="Enter Last Name" />
+                            </div>
+                            <div className="mb-3 col-md-12">
+                                <label>Password<span className="text-danger">*</span></label>
+                                <input type="password" name="password" className="form-control" placeholder="Enter Password" />
+                            </div>
+
+                            <div className="mb-3 col-md-12">
+                                <label>Confirm Password<span className="text-danger">*</span></label>
+                                <input type="password" name="confirmpassword" className="form-control" placeholder="Confirm Password" />
+                            </div>
+                            <div className="col-md-12">
+                                <button className="btn btn-primary float-end">Signup Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    <p className="text-center mt-3 text-secondary">If you have account, Please <Link to="/signin">Login Now</Link></p>
+                </div>
+            </div>
+        </div>
+    </div>)
 
 }
 
