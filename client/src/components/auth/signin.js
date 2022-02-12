c439d2a4777ae8b4a2b78ef96a93f6ea209d75ad
debasishsahoo96 <debasish.sahoo96@gmail.com>
2022-02-12 17:24:38 +0530

    react 1

--- client/src/components/auth/signin.js
+++ client/src/components/auth/signin.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-
+import Axios from 'axios'
 
 function SignIn() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const login = async e => {
+        e.preventDefault()
+        if (password !== "" && email !== "") {
+            try {
+                await Axios.post('http://localhost:7000/user/signin', {
+                    email, password
+                });
+                localStorage.setItem('profile', true)
+                //window.location.href = '/post'
+            }
+            catch (err) {
+                alert(err.response.data.message)
+            }
+        } else {
+            alert('Please Provide Password Correctly')
+        }
+    }
+
     return (<div className="container">
         <div className="row">
             <div className="col-md-6 offset-md-3">
@@ -12,14 +32,14 @@ function SignIn() {
                         <div className="row">
                             <div className="mb-3 col-md-12">
                                 <label>Email<span className="text-danger">*</span></label>
-                                <input type="email" name="email" className="form-control" placeholder="Enter Email" />
+                                <input type="email" name="email" className="form-control" placeholder="Enter Email" onChange={(e) => { setEmail(e.target.value); }} />
                             </div>
                             <div className="mb-3 col-md-12">
                                 <label>Password<span className="text-danger">*</span></label>
-                                <input type="password" name="password" className="form-control" placeholder="Enter Password" />
+                                <input type="password" name="password" className="form-control" placeholder="Enter Password" onChange={(e) => { setPassword(e.target.value); }} />
                             </div>
                             <div className="col-md-12">
-                                <button className="btn btn-primary float-end">Signin Now</button>
+                                <button className="btn btn-primary float-end" onClick={login}>Signin Now</button>
                             </div>
                         </div>
                     </div>
